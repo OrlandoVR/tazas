5f0f6955ef2cbf06131d8c234c145577569c635f
Orlando <variorlando1@gmail.com>
2025-07-13 20:22:58 +0200

    corrigiendo rutas 4

--- js/navigator.js
+++ js/navigator.js
@@ -1,20 +1,33 @@
 
-  document.addEventListener("DOMContentLoaded", ()=>{
-    
-    const navigatorEl = document.getElementById("nav-placeholder");
+document.addEventListener("DOMContentLoaded", () => {
 
-    let navigatorPath = "partials/navigator.html";
-    if (window.location.pathname.includes("/pages/")) {
-      navigatorPath = "../partials/navigator.html";
-    }
+  const navigatorEl = document.getElementById("nav-placeholder");
+  const isInPages = window.location.pathname.includes("/pages/");
 
-    fetch(navigatorPath)
-    .then(res => res.text())
-    .then(html => {
-      navigatorEl.innerHTML = html;
-    })
-    .catch(err => {
-      console.error("Error cargando navigator:", err);
-    });
+  const basePath = isInPages ? "../" : "";
 
-  });
+  navigatorEl.innerHTML = `
+      <nav class="navegacion">
+        <div class="navegacion__contenedor">
+          <a href="${basePath}index.html" class="navegacion__logo">
+            <img src="${basePath}images/logo.png" alt="Logo" height="50px">
+          </a>
+
+          <input type="checkbox" id="menu-toggle" class="menu-toggle">
+          <label for="menu-toggle" class="menu-icon">
+            <i class="fas fa-bars"></i>
+          </label>
+
+        <div class="navegacion__menu">
+            <ul id="lista-opciones">
+            <li><a href="${basePath}index.html">Inicio</a></li>
+            <li><a href="${basePath}pages/productos.html">Productos</a></li>
+            <li><a href="#">Presupuesto</a></li>
+            <li><a href="#">Contacto</a></li>
+            </ul>
+        </div>
+        </div>
+      </nav>      
+      `
+});
+
